f43217ed45b0505d5b1216b0da311887aec29b63
Jon Smithers <jsmithe2@gmu.edu>
2014-03-02 09:46:57 -0500

    Polished up code so it can be tested on Arduino

--- main.py
+++ main.py
@@ -1,28 +1,60 @@
 # def main(filename):
 
-while (true):
+import urllib2
+import time
+import calendar
+import datetime
+import math
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+
+def computeBrightness(initialTime, curTime, cycleTime):
+    
+    if curTime<initialTime:
+        raise Exception("Value Error")
+    else:
+        curTime -= 5*60*60 #adjust for timezone, so that brightness peaks at noon (local time)
+        curTime = curTime % cycleTime   # no need to compute with super large numbers
+        
+        brightness = 2*math.pi
+
+        brightness = brightness / cycleTime
+        
+        brightness = brightness * curTime
+
+        brightness = math.sin(brightness)
+
+        brightness = brightness + 1
+
+        brightness = brightness * 255/2
+        brightness = int(brightness)
+        brightness = str(brightness)
+        brightness = 'b'+brightness
+
+        return brightness
+
+
+while (True):
     command = urllib2.urlopen("http://fluxbulb.hackmason.org:3000/arduinoCommand").read()
 
     comChar = command[0]
 
+    curTime = calendar.timegm(datetime.datetime.now().utctimetuple())
 
     if (comChar == 's'):
-            brightness = computeBrightness(0,calendar.timegm(datetime.datetime.now().utctimetuple()),86400)
-            print "Standard Brightness"
-
-    
+            brightness = computeBrightness(0,curTime,86400)
     elif (comChar == 'i'):
-            #set brightness to computeBrightness(...)				#"b0" "b143" "b255"
             duration = int(command[1:])
-            brightness = computeBrightness(0,calendar.timegm(datetime.datetime.now().utctimetuple()),duration)
-            print "Set Duration"
+            brightness = computeBrightness(0,curTime,duration)
     elif (comChar == 'c'):
-            #set brightness to level 
-            level = int(command[1:])								#"b12" "b12" "b12" "b12"
+            level = int(command[1:])
             brightness = level
-            print "Set Level"
-	else
-		print("COULD NOT IDENTIFY")    
-    }
+    else:
+        print("COULD NOT IDENTIFY")    
+    
 
+    ser.write(b+brightness)
     print brightness
+    time.sleep(1)
